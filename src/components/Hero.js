853dc95f0c72ef5906dd224f8f0b835b0b1080dd
patrick <patrick.treppmann97@gmail.com>
2023-04-20 18:56:20 +0200

    hero page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,9 @@ import styles from './Hero.module.css';
 const Hero = () => {
     return (
         <section className={styles["hero"]}>
-
+            <div className={styles["hero-inner-layout"]}>
+                <h1>hello</h1>
+            </div>
         </section>
     );
 }
